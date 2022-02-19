e9cef4cb653b8d8501492cbab0a0ffe74b548fcf
sergheusz <nullfuzz@duck.com>
2022-02-19 20:02:54 -0300

    Se agregan dependencias y avances en main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env{
+    env,
+    error::Error,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        return Ok(()); 
+    }
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+    return Err("sha1 hash is not valid".into()); }
+    Ok(())
 }
